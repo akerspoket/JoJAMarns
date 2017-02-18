c018b4ed4893b53aa6fe32a6a25b415181c0c1fa
Jawsarn <christian_markowicz@hotmail.com>
2017-02-18 12:06:27 +0100

    Started on respawn system

---
+++ Wilcox/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+    public bool isInfinityPlane = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log("Dead");
+
+        PlayerHealth health = other.gameObject.GetComponent<PlayerHealth>();
+        if(isInfinityPlane)
+            health.KillByFall();
+        else
+        {
+            // Spike trap
+
+        }
+    }
+}
---
+++ Wilcox/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    // Should be checked by movement components to interrupt new movements
+    public bool isDead = false;
+    public GameObject checkpoint = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isDead)
+        {
+            // Play death animation(?)
+
+            // Check if death animation ended
+            if(true)
+            {
+                // Respawn player
+                if(checkpoint != null)
+                {
+                    transform.position = checkpoint.transform.position;
+                    transform.rotation = checkpoint.transform.rotation;
+                    isDead = false;
+                }
+            }
+            
+        }
+	}
+
+    // Used for infinity fall(animation etc)
+    public void KillByFall()
+    {
+        isDead = true;
+        // Start death animation
+
+        Debug.Log("Dead");
+    }
+}
