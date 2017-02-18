fa37bb4ed12aec24d8f7b2299db87218c298ca53
asfasdasd <asda>
2017-02-18 19:21:45 +0100

    Lagt till two maps plus pendulum base

---
+++ Wilcox/Assets/Scripts/PendulumMotion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PendulumMotion : MonoBehaviour {
+
+    // Use this for initialization
+    Transform baseGameObject;
+    Transform endGameObject;
+    Transform[] allGameObjects;
+	void Start ()
+    {
+
+        allGameObjects = gameObject.GetComponentsInChildren<Transform>();
+
+        foreach (var item in allGameObjects)
+        {
+            if (item.name== "PendBase")
+            {
+                baseGameObject = gameObject.GetComponentInChildren<Transform>();
+                print("Hittat basePend");
+            }
+            if(item.name == "PendEnd")
+            {
+                endGameObject = gameObject.GetComponentInChildren<Transform>();
+                print("Hittat endPend");
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Wilcox/Assets/Scripts/Teleport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+    public GameObject teleportObj = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(teleportObj != null)
+        {
+            // check if obj is player? 
+            other.transform.position = teleportObj.transform.position;
+            other.transform.rotation = teleportObj.transform.rotation;
+        }
+        else
+        {
+            Debug.LogError("No teleport object set");
+        }
+    }
+}
